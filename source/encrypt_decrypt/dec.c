d80540278b5151984271f36725900ad2a10dc0b7
AimTao <net@aimtao.net>
2020-05-03 02:13:41 +0800

    Creat enc&dec

---
+++ source/encrypt_decrypt/dec.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+    // 读入待解密文件路径
+    char enc_path[512] = {0};
+    char backup_enc_path[512] = {0};
+    printf("请输入待解密文件路径（例如: ../enc/hello.jpg）：");
+    scanf("%s", enc_path);
+    strcpy(backup_enc_path, enc_path);
+
+
+    // 生成解密文件路径
+    char dec_path[512] = {0};
+    char tmp_last_path[100] = {0};
+    char *strtok_p = NULL;
+    strtok_p = strtok(backup_enc_path, ".");
+    if (strtok_p != NULL) {
+        while (strtok_p != NULL) {
+            strcpy(tmp_last_path, strtok_p);
+            strtok_p = strtok(NULL, ".");
+        }
+        sprintf(dec_path, "%s_your_dec.%s", enc_path, tmp_last_path);
+    } else {
+        sprintf(dec_path, "%s_your_dec", enc_path);
+    }
+
+    // 打开文件
+    FILE *enc_fp = NULL;
+    FILE *dec_fp = NULL;
+    enc_fp = fopen(enc_path, "rb+");
+    if (enc_fp == NULL) {
+        perror("\n【打开待解密文件失败！】\n 原因：");
+        return 0;
+    }
+    dec_fp = fopen(dec_path, "wb+");
+    if (dec_fp == NULL) {
+        perror("\n【解密失败！\n 原因：");
+        return 0;
+    }
+
+    // 读取密钥
+    long num_key = 0;
+    fread(&num_key, 1, sizeof(num_key), enc_fp);
+    char key[100] = {0};
+    sprintf(key, "%ld", num_key * 13 - 20200502);
+    int key_lens = strlen(key);
+    int size_key = num_key % 5 * 1024 + 1024;
+    
+    // 边读边写，解密
+    while (1) {
+        char tmp_enc[6144] = {0};      // 1024 * 5
+        int read_size = fread(tmp_enc, 1, size_key, enc_fp);
+        if (!read_size) {
+            break;
+        }
+        int enc_lens = strlen(tmp_enc);
+        for (int i = 0; i < enc_lens; i++) {
+            tmp_enc[i] = tmp_enc[i] ^ key[i % key_lens];
+        }
+        fwrite(tmp_enc, 1, size_key, dec_fp);
+    }
+
+    // 关闭文件
+    fclose(enc_fp);
+    fclose(dec_fp);
+    
+    return 0;
+}
---
+++ source/encrypt_decrypt/enc.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main() {
+
+    // 获取待加密文件路径
+    char source_path[512] = {0};
+    char backup_source_path[512] = {0};    printf("请输入待加密文件路径（例如: ../enc/hello.jpg）：");
+    scanf("%s", source_path);
+    strcpy(backup_source_path, source_path);
+    
+    // 生成已加密文件路径
+    char enc_path[512] = {0};
+    char tmp_last_path[100] = {0};  // 获取文件后缀名
+    char *strtok_p = strtok(backup_source_path, ".");
+    if (strtok_p != NULL) {
+        while (strtok_p != NULL) {
+            strcpy(tmp_last_path, strtok_p);
+            strtok_p = strtok(NULL, ".");
+        }
+        sprintf(enc_path, "%s_your_enc.%s", source_path, tmp_last_path);
+    } else {
+        sprintf(enc_path, "%s_your_enc", source_path);
+    }
+
+    // 打开文件
+    FILE *source_fp = NULL;
+    FILE *enc_fp = NULL;
+    source_fp = fopen(source_path, "rb");
+    if (source_fp == NULL) {
+        perror("\n【打开待加密文件失败！】\n 原因：");
+        return 0;
+    }
+    enc_fp = fopen(enc_path, "wb+");
+    if (source_fp == NULL) {
+        perror("\n【加密失败！】\n 原因：");
+        return 0;
+    }
+
+    // 用当前时间做随机量，设计随机密钥、随机块大小
+    long num_key = time(NULL);
+    char key[100] = {0};
+    sprintf(key, "%ld", num_key * 13 - 20200502);
+    int key_lens = strlen(key);
+    int size_key = num_key % 5 * 1024 + 1024; 
+    // 写入解密密钥
+    fwrite(&num_key, 1, sizeof(num_key), enc_fp);
+    
+    // 边读边写，加密
+    while (1) {
+        char tmp_source[6144] = {0};    // 1024 * 5
+        int read_size = fread(tmp_source, 1, size_key, source_fp);
+        if (!read_size) {
+            break;
+        }
+        int source_lens = strlen(tmp_source);
+        for (int i = 0; i < source_lens; i++) {
+            tmp_source[i] = tmp_source[i] ^ key[i % key_lens];
+        }
+        fwrite(tmp_source, 1, size_key, enc_fp);
+    }
+
+    // 关闭文件
+    fclose(source_fp);
+    fclose(enc_fp);
+
+    return 0;
+}
+
