01120476809002f5c437b4b55c7f7b51b9a86629
AimTao <net@aimtao.net>
2020-05-04 01:56:08 +0800

    Add decrypt

--- source/encrypt_decrypt/dec.c
+++ source/encrypt_decrypt/dec.c
@@ -2,42 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
+int main(int argc, char **argv) {
 
     // 读入待解密文件路径
-    char enc_path[512] = {0};
+    char *enc_path = argv[1];
     char backup_enc_path[512] = {0};
-    printf("请输入待解密文件路径（例如: ../enc/hello.jpg）：");
-    scanf("%s", enc_path);
     strcpy(backup_enc_path, enc_path);
 
-
-    // 生成解密文件路径
-    char dec_path[512] = {0};
-    char tmp_last_path[100] = {0};
-    char *strtok_p = NULL;
-    strtok_p = strtok(backup_enc_path, ".");
-    if (strtok_p != NULL) {
-        while (strtok_p != NULL) {
-            strcpy(tmp_last_path, strtok_p);
-            strtok_p = strtok(NULL, ".");
-        }
-        sprintf(dec_path, "%s_your_dec.%s", enc_path, tmp_last_path);
-    } else {
-        sprintf(dec_path, "%s_your_dec", enc_path);
-    }
-
-    // 打开文件
+    // 打开读文件，检查路径
     FILE *enc_fp = NULL;
-    FILE *dec_fp = NULL;
     enc_fp = fopen(enc_path, "rb+");
     if (enc_fp == NULL) {
         perror("\n【打开待解密文件失败！】\n 原因：");
         return 0;
     }
+
+    // 生成解密文件路径
+    char dec_path[512] = {0};
+    char last_name[100] = {0};    // 获取文件后缀名
+    char file_name[100] = {0};   // 获取文件名（省去后缀名）
+    char *strtok_p = NULL;
+    strtok_p = strtok(backup_enc_path, ".");
+    strtok_p = strtok(NULL, ".");
+    if (strtok_p != NULL) {
+        while (strtok_p != NULL) {
+            strcpy(last_name, strtok_p);
+            strtok_p = strtok(NULL, ".");
+        }
+        char *last_path_ptr = NULL;
+        char *enc_path_ptr = enc_path;
+        while (1) {
+            last_path_ptr = strstr(enc_path_ptr, last_name);
+            if (last_path_ptr == NULL) {
+                enc_path_ptr -= strlen(last_name);
+                break;
+            } else {
+                enc_path_ptr = last_path_ptr + strlen(last_name);
+                if (enc_path_ptr > enc_path + strlen(enc_path)) {
+                    break;
+                }
+            }
+        }
+        strncpy(file_name, enc_path, enc_path_ptr - 1 - enc_path);
+        sprintf(dec_path, "%s_dec.%s", file_name, last_name);
+    } else {
+        sprintf(dec_path, "%s_dec", enc_path);
+    }
+
+    // 打开写文件
+    FILE *dec_fp = NULL;
     dec_fp = fopen(dec_path, "wb+");
     if (dec_fp == NULL) {
-        perror("\n【解密失败！\n 原因：");
+        perror("\n【解密失败！】\n 原因：");
         return 0;
     }
 
@@ -61,6 +77,7 @@ int main() {
         }
         fwrite(tmp_enc, 1, read_size, dec_fp);
     }
+    printf("\n\n解密已完成！解密文件为于 %s\n\n", dec_path);
 
     // 关闭文件
     fclose(enc_fp);
