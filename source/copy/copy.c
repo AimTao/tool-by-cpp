0cd4166b791cc9dfe170b9644fa16993f5a0b534
AimTao <net@aimtao.net>
2020-05-06 17:36:13 +0800

    Fix the same name

--- source/copy/copy.c
+++ source/copy/copy.c
@@ -2,15 +2,19 @@
 #include <string.h>
 
 int main(int argc, char** argv) {
+    if (!strcmp(argv[1], argv[2])) {
+        printf("error: two files with the same name！");
+        return 0;
+    }
     FILE *source_fp = NULL;
     FILE *copy_fp = NULL;
     source_fp = fopen(argv[1], "rb+");
     if (source_fp == NULL) {
-        perror("打开文件失败：");
+        perror("open file failed:");
     }
     copy_fp  = fopen(argv[2], "wb+");
     if (copy_fp == NULL) {
-        perror("复制文件失败：");
+        perror("copy file failed:");
     }
 
     int res = 1;
